b7fc13ac9fec61a45f961af9991ea789c6871d01
Alexander Koksahrov <akokshar@redhat.com>
2017-02-04 18:39:17 +0100

    ,,,

---
+++ StatusLineNetworkManager.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from Logger import Logger
+from StatusLineControl import StatusLineControl
+from StatusLineBlock import StatusLineBlock
+
+class StatusLineNetworkManager(StatusLineControl):
+
+    def __init__():
+        StatusLineControl.__init__(self, "nm")
+
